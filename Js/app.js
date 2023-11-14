6aca63e7134f846b506421eb854870ee35e8fe01
Rakib Hosain <143322856+RakibHosain2302@users.noreply.github.com>
2023-11-14 23:54:56 +0600

    Add files via upload

---
+++ Js/app.js
@@ -0,0 +1,12 @@
+
+$(function(){
+    $(".slider").slick({
+        slidesToShow: 1,
+        slidesToScroll:2,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        infinite: true,
+       
+    })
+})
